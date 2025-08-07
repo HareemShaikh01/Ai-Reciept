29b0c60a5641e8f6c74d339c41bf86481a873cd9
HareemShaikh01 <hareemshaikh2006@gmail.com>
2025-08-07 15:41:10 +0500

    almost done

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from app.routes.workspace import workspace_bp
 from app.routes.categories import categories_dp
 from app.routes.reciepts import reciepts_bp
 from app.routes.transactions import transaction_bp
+from app.routes.reports import report_bp
+from app.routes.insights import insights_bp
 from app.utils.save_reciept_image import save_receipt_image
 from app.utils.reciept_parser import reciept_parser
 
@@ -13,6 +15,8 @@ app.register_blueprint(workspace_bp)
 app.register_blueprint(categories_dp)
 app.register_blueprint(reciepts_bp)
 app.register_blueprint(transaction_bp)
+app.register_blueprint(report_bp)
+app.register_blueprint(insights_bp)
 
 @app.route("/")
 def runApp():
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-from app.utils.query_transactions import query_transactions
-from app.services.transactions import get_budget_utilisation
+from app.services.graphs import get_bar_chart_data ,get_line_chart_data,get_pie_chart_data
+from app.utils.query_transactions import get_category_map
+from app.utils.llm_advice import llm_advice
+from app.services.reports import instance_report
 
-
-print(get_budget_utilisation('d98e23b2-6570-43b1-8570-baed81d3cf21'))
+print(llm_advice('bb007a2b-a94e-4e74-bf1a-0e807fb92f72'))
